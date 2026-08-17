093b4f6bc42e3cd3567f7f5a29ee8d6fb09f7184
rajatk-spektra <rajat.kumar@spektrasystems.com>
2026-08-18 00:21:10 +0530

    Create psscript-01.ps1

---
+++ psscript-01.ps1
@@ -0,0 +1,105 @@
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
+
+    [string]
+    $AzurePassword,
+
+    [string]
+    $AzureTenantID,
+
+    [string]
+    $AzureSubscriptionID,
+
+    [string]
+    $ODLID,
+
+    [string]
+    $DeploymentID,
+
+    [string]
+    $vmAdminUsername,
+
+    [string]
+    $adminPassword,
+
+    [string]
+    $trainerUserName,
+
+    [string]
+    $trainerUserPassword
+)
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+Write-Host "Adding .env variables"
+[System.Environment]::SetEnvironmentVariable('AzureTenantID', $AzureTenantID,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('AzureSubscriptionID', $AzureSubscriptionID,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('DeploymentID', $DeploymentID,[System.EnvironmentVariableTarget]::Machine)
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+#CloudlabsManualAgent Install
+CloudLabsManualAgent Install
+
+InstallChocolatey
+refreshenv
+choco install git -y
+refreshenv
+choco install vscode.install -y
+choco install python --version=3.11.0
+
+# Run Imported functions from cloudlabs-windows-functions.ps1
+WindowsServerCommon
+
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID
+
+Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
+
+Function InstallModernVmValidator
+{    
+    Remove-Item C:\CloudLabs\Validator -Recurse -Force
+    #dotnet core is pre-req for vmagent or validator
+    choco install dotnetcore -y -force
+    #Create C:\CloudLabs\Validator directory
+    New-Item -ItemType directory -Path C:\CloudLabs\Validator -Force
+    Invoke-WebRequest 'https://experienceazure.blob.core.windows.net/software/vm-validator/VMAgent.zip' -OutFile 'C:\CloudLabs\Validator\VMAgent.zip'
+    Expand-Archive -LiteralPath 'C:\CloudLabs\Validator\VMAgent.zip' -DestinationPath 'C:\CloudLabs\Validator' -Force
+    Set-ExecutionPolicy -ExecutionPolicy bypass -Force
+    cmd.exe --% /c @echo off
+    cmd.exe --% /c sc create "Spektra CloudLabs VM Agent" BinPath=C:\CloudLabs\Validator\VMAgent\Spektra.CloudLabs.VMAgent.exe start= auto
+    cmd.exe --% /c sc start "Spektra CloudLabs VM Agent"
+}
+InstallModernVmValidator
+
+#Download LogonTask
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/ai-foundry-and-semantic-kernel/scripts/logontask-01.ps1","C:\LabFiles\logontask-01.ps1")
+
+#Enable Auto-Logon
+$AutoLogonRegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoAdminLogon" -Value "1" -type String
+Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultUsername" -Value "$($env:ComputerName)\demouser" -type String
+Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultPassword" -Value $adminPassword -type String
+Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoLogonCount" -Value "1" -type DWord
+
+# Scheduled Task
+$Trigger= New-ScheduledTaskTrigger -AtLogOn
+$User= "$($env:ComputerName)\demouser"
+$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File C:\LabFiles\logontask-01.ps1"
+Register-ScheduledTask -TaskName "Setup" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
+
+$Validstatus="Pending"
+$Validmessage="Post Deployment is Pending"
+
+#Set the final deployment status
+CloudlabsManualAgent setStatus
+
+Stop-Transcript
+Restart-Computer -Force 
